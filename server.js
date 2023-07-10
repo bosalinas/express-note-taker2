b07ec0efd0b319cc1f05db2200652ff28d257078
Freebird <bo.salinas12@gmail.com>
2023-07-10 14:08:26 -0500

    git ignore added with package.json and readme updated

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //required 
 const express = require('express');
 const path = require('path');
-const api = require("./db/db.json");
 const app = express();
 const fs = require('fs');
 
@@ -22,15 +21,15 @@ app.get('/api/notes', (req, res) => {
     //read file add to get post line 23
     let notes = JSON.parse(fs.readFileSync('./db/db.json'))
     res.json(notes);
-    console.log("working");
 });
 
 //POST to push db.json file to body and show it as a string
 app.post('/api/notes', (req, res) => {
-    req.body.id = api.length + 1
-    api.push(req.body)
-    fs.writeFileSync("./db/db.json", JSON.stringify(api))
-    res.json(api);
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    req.body.id = notes.length + 1
+    notes.push(req.body)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
